8c42883cc1d6d0c025cf8952c4db0ddd2a1e7d9e
vprahaladhan <vprahaladhan@users.noreply.github.com>
2020-01-10 09:33:33 +0800

    Updates

---
+++ precache-manifest.043d5e9fc954aa74e8966ee767f2bedd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf270534e7b58336489dc69a1677a0c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "55d71e804d0f8ed0b0c9",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7cab3c914cccdf8eff53",
+    "url": "/robofriends/static/css/main.97c3d749.chunk.css"
+  },
+  {
+    "revision": "55d71e804d0f8ed0b0c9",
+    "url": "/robofriends/static/js/2.c9b1fbec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.c9b1fbec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cab3c914cccdf8eff53",
+    "url": "/robofriends/static/js/main.cab9f827.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  }
+]);
